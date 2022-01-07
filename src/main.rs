c0a7970a34c36bdc08f81d8cc8d81ccedd4f4abf
oxalica <oxalicc@pm.me>
2022-01-08 01:06:19 +0800

    Switch to pico-args and add parsing tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-only
 use std::convert::TryInto;
-use std::ffi::CString;
+use std::ffi::{CString, OsString};
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
-use std::{fs, process};
-use structopt::clap::{Error as ClapError, ErrorKind as ClapErrorKind};
-use structopt::StructOpt;
+use std::process;
 
-#[derive(Debug)]
+use anyhow::{anyhow, bail, ensure, Result};
+use pico_args::Arguments;
+
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
 struct App {
     force: bool,
     no_clobber: bool,
@@ -16,87 +17,124 @@ struct App {
     operations: Vec<(PathBuf, PathBuf)>,
 }
 
-/// mv(1) but without cp(1) fallback. Simple wrapper of rename(2)/renameat2(2).
-#[derive(StructOpt, Debug)]
-#[structopt(
-    usage = "\
+impl App {
+    fn help() -> String {
+        format!(
+            "\
+rawmv {version}
+mv(1) but without cp(1) fallback. Simple wrapper of rename(2)/renameat2(2).
+
+USAGE:
     rawmv [OPTION]... [-T] <SOURCE> <DEST>
     rawmv [OPTION]... <SOURCE>... <DIRECTORY>
-    rawmv [OPTION]... -t <DIRECTORY> <SOURCE>...\
-",
-    after_help = "\
+    rawmv [OPTION]... -t <DIRECTORY> <SOURCE>...
+
+FLAGS:
+    -f, --force                 Do not prompt before overwriting. Note that
+                                unlike mv(1), without this flag, we raise an
+                                error if the destination already exists
+    -h, --help                  Prints help informatio.
+    -i, --interactive           Prompt for confirmation before overwrite
+    -n, --no-clobber            Silently skip files whose destinations exist
+    -T, --no-target-directory   Always treat the last path (destination) as a
+                                normal file. This implies that only two
+                                operands are expected
+    -V, --version               Prints version information
+    -v, --verbose               Print what is being done
+
+OPTIONS:
+    -t, --target-directory <DIRECTORY>  Move all files into this directory
+
 Copyright (C) 2021 oxalica<oxalicc@pm.me>
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-"
-)]
-struct RawOpt {
-    /// Do not prompt before overwriting.
-    /// Note that unlike mv(1), without this flag, we raise error if destination exists.
-    #[structopt(long, short)]
-    force: bool,
-    /// Silently skip files whose destination exists.
-    #[structopt(long, short, conflicts_with_all = &["force", "interactive"])]
-    no_clobber: bool,
-    /// Prompt before overwrite.
-    #[structopt(long, short, conflicts_with = "force")]
-    interactive: bool,
-    /// Print what is being done.
-    #[structopt(long, short)]
-    verbose: bool,
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, version 3.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+",
+            version = env!("CARGO_PKG_VERSION")
+        )
+    }
 
-    /// Always treat the last path (destination) as a normal file.
-    /// This implies that only two operands are expected.
-    #[structopt(long, short = "T")]
-    no_target_directory: bool,
-    /// Move all files into this directory.
-    #[structopt(
-        long,
-        short = "t",
-        name = "DIRECTORY",
-        conflicts_with = "no-target-directory"
-    )]
-    target_directory: Option<PathBuf>,
+    fn parse_env() -> Result<Self> {
+        Self::parse_args(std::env::args_os().skip(1))
+    }
 
-    #[structopt(hidden = true)]
-    files: Vec<PathBuf>,
-}
-
-impl App {
-    fn parse_args() -> Result<Self, ClapError> {
-        let mut opt = RawOpt::from_args_safe()?;
-        let mut this = Self {
-            force: opt.force,
-            no_clobber: opt.no_clobber,
-            interactive: opt.interactive,
-            verbose: opt.verbose,
-            operations: Vec::with_capacity(opt.files.len()),
+    fn parse_args<I: IntoIterator<Item = S>, S: Into<OsString>>(args: I) -> Result<Self> {
+        let mut raw_args = args
+            .into_iter()
+            .map(|s| s.into())
+            .collect::<Vec<OsString>>();
+        let tail_positionals = match raw_args.iter().position(|s| s == "--") {
+            None => Vec::new(),
+            Some(pos) => {
+                let tail = raw_args.drain(pos + 1..).collect();
+                raw_args.pop();
+                tail
+            }
         };
 
-        let wrong_arg_num = |s| ClapError::with_description(s, ClapErrorKind::WrongNumberOfValues);
+        let mut args = Arguments::from_vec(raw_args);
 
-        if opt.no_target_directory {
-            if opt.files.len() != 2 {
-                return Err(wrong_arg_num("-T expects exact 2 file operands"));
-            }
-            let [src, dest]: [_; 2] = opt.files.try_into().unwrap();
-            this.push_move_to_target(src, dest)?;
-        } else if let Some(target_dir) = opt.target_directory {
-            if opt.files.is_empty() {
-                return Err(wrong_arg_num("Missing file operand"));
-            }
-            this.push_move_to_dir(opt.files, &target_dir)?;
+        if args.contains(["-h", "--help"]) {
+            print!("{}", Self::help());
+            process::exit(0);
+        }
+
+        if args.contains(["-V", "--version"]) {
+            println!("rawmv {}", env!("CARGO_PKG_VERSION"));
+            process::exit(0);
+        }
+
+        let mut this = Self {
+            force: args.contains(["-f", "--force"]),
+            no_clobber: args.contains(["-n", "--no-clobber"]),
+            interactive: args.contains(["-i", "--interactive"]),
+            verbose: args.contains(["-v", "--verbose"]),
+            operations: Vec::new(),
+        };
+        let target_directory = args
+            .opt_value_from_os_str::<_, PathBuf, String>(["-t", "--target-directory"], |s| {
+                Ok(s.to_os_string().into())
+            })?;
+        let no_target_directory = args.contains(["-T", "--no-target-directory"]);
+
+        ensure!(
+            !this.force || !this.no_clobber,
+            "'-f' and '-n' are exclusive"
+        );
+        ensure!(
+            target_directory.is_none() || !no_target_directory,
+            "'-T' and '-t' are exclusive"
+        );
+
+        let mut positionals = args
+            .finish()
+            .into_iter()
+            .chain(tail_positionals)
+            .map(|path| path.into())
+            .collect::<Vec<PathBuf>>();
+
+        if no_target_directory {
+            let [src, dest]: [_; 2] = positionals
+                .try_into()
+                .map_err(|_| anyhow!("'-T' expects exact 2 file operands"))?;
+            this.operations.push((src, dest));
+        } else if let Some(target_dir) = target_directory {
+            ensure!(!positionals.is_empty(), "Missing file operand");
+            this.push_move_to_dir(positionals, &target_dir)?;
         } else {
-            match opt.files.len() {
-                0 => return Err(wrong_arg_num("Missing file operand")),
-                1 => return Err(wrong_arg_num("Missing destination operand")),
-                2 if !opt.files[1].is_dir() => {
-                    let [src, dest]: [_; 2] = opt.files.try_into().unwrap();
-                    this.push_move_to_target(src, dest)?;
+            match positionals.len() {
+                0 => bail!("Missing file operand"),
+                1 => bail!("Missing destination operand"),
+                2 if !positionals[1].is_dir() => {
+                    let [src, dest]: [_; 2] = positionals.try_into().unwrap();
+                    this.operations.push((src, dest));
                 }
                 _ => {
-                    let target_dir = opt.files.pop().unwrap();
-                    this.push_move_to_dir(opt.files, &target_dir)?;
+                    let target_dir = positionals.pop().unwrap();
+                    this.push_move_to_dir(positionals, &target_dir)?;
                 }
             }
         }
@@ -104,44 +142,27 @@ impl App {
         Ok(this)
     }
 
-    fn push_move_to_target(&mut self, src: PathBuf, dest: PathBuf) -> Result<(), ClapError> {
-        fs::symlink_metadata(&src).map_err(|_| {
-            ClapError::with_description(
-                &format!("File not found: {:?}", src),
-                ClapErrorKind::InvalidValue,
-            )
-        })?;
-        self.operations.push((src, dest));
-        Ok(())
-    }
-
     fn push_move_to_dir(
         &mut self,
         srcs: impl IntoIterator<Item = PathBuf>,
         target_dir: &Path,
-    ) -> Result<(), ClapError> {
-        if !target_dir.is_dir() {
-            return Err(ClapError::with_description(
-                &format!("Not a directory: {:?}", target_dir),
-                ClapErrorKind::InvalidValue,
-            ));
-        }
+    ) -> Result<()> {
         for src in srcs {
-            let name = src.file_name().ok_or_else(|| {
-                ClapError::with_description(
-                    &format!("Path doesn't have base name: {:?}", src),
-                    ClapErrorKind::InvalidValue,
-                )
-            })?;
-            let dest = target_dir.join(name);
-            self.push_move_to_target(src, dest)?;
+            let base = src
+                .file_name()
+                .ok_or_else(|| anyhow!("Source path doesn't have base name: {}", src.display()))?;
+            let dest = target_dir.join(base);
+            self.operations.push((src, dest));
         }
         Ok(())
     }
 }
 
 fn main() {
-    let app = App::parse_args().unwrap_or_else(|err| ClapError::exit(&err));
+    let app = App::parse_env().unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
 
     let mut failed = false;
     for (src, dest) in &app.operations {
@@ -199,5 +220,154 @@ fn do_rename(src: &Path, dest: &Path, overwrite: bool) -> io::Result<()> {
         Ok(())
     } else {
         Err(io::Error::last_os_error())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::App;
+
+    fn parse(args: &[&str]) -> Result<App, String> {
+        App::parse_args(args.iter()).map_err(|e| e.to_string())
+    }
+
+    #[test]
+    fn test_parse_auto_detect() {
+        assert_eq!(
+            parse(&["/non/existing/file", "/"]).unwrap(),
+            App {
+                operations: vec![("/non/existing/file".into(), "/file".into())],
+                ..App::default()
+            }
+        );
+        assert_eq!(
+            parse(&["/non/existing/file", "/non/existing/other"]).unwrap(),
+            App {
+                operations: vec![("/non/existing/file".into(), "/non/existing/other".into())],
+                ..App::default()
+            }
+        );
+
+        assert_eq!(
+            parse(&["/foo", "/bar", "/non/existing"]).unwrap(),
+            App {
+                operations: vec![
+                    ("/foo".into(), "/non/existing/foo".into()),
+                    ("/bar".into(), "/non/existing/bar".into())
+                ],
+                ..App::default()
+            }
+        );
+
+        assert_eq!(parse(&[]).unwrap_err(), "Missing file operand",);
+        assert_eq!(parse(&["foo"]).unwrap_err(), "Missing destination operand",);
+    }
+
+    #[test]
+    fn test_parse_no_target_dir() {
+        assert_eq!(
+            parse(&["-T", "/", "/"]).unwrap(),
+            App {
+                operations: vec![("/".into(), "/".into())],
+                ..App::default()
+            }
+        );
+        assert_eq!(
+            parse(&["-T", "/"]).unwrap_err(),
+            "'-T' expects exact 2 file operands",
+        );
+        assert_eq!(
+            parse(&["-T", "/", "/", "/"]).unwrap_err(),
+            "'-T' expects exact 2 file operands",
+        );
+    }
+
+    #[test]
+    fn test_parse_target_dir() {
+        assert_eq!(parse(&["-t", "/"]).unwrap_err(), "Missing file operand",);
+        assert_eq!(
+            parse(&["-T", "-t", "/"]).unwrap_err(),
+            "'-T' and '-t' are exclusive",
+        );
+        assert_eq!(
+            parse(&["foo", "-t"]).unwrap_err(),
+            "the '-t' option doesn't have an associated value"
+        );
+
+        assert_eq!(
+            parse(&["/some/non/existing/file", "-t", "/"]).unwrap(),
+            App {
+                operations: vec![("/some/non/existing/file".into(), "/file".into())],
+                ..App::default()
+            }
+        );
+        assert_eq!(
+            parse(&["-t", "foo", "bar", "baz"]).unwrap(),
+            App {
+                operations: vec![
+                    ("bar".into(), "foo/bar".into()),
+                    ("baz".into(), "foo/baz".into())
+                ],
+                ..App::default()
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_clobber_flags() {
+        let app = App {
+            operations: vec![("foo".into(), "/foo".into())],
+            ..App::default()
+        };
+
+        assert_eq!(
+            parse(&["-n", "foo", "/"]).unwrap(),
+            App {
+                no_clobber: true,
+                ..app.clone()
+            },
+        );
+        assert_eq!(
+            parse(&["-f", "foo", "/"]).unwrap(),
+            App { force: true, ..app },
+        );
+
+        assert_eq!(
+            parse(&["-f", "foo", "/", "-n"]).unwrap_err(),
+            "'-f' and '-n' are exclusive"
+        );
+    }
+
+    #[test]
+    fn test_parse_dash_dash() {
+        assert_eq!(
+            parse(&["-f", "foo", "--", "-n", "-t"]).unwrap(),
+            App {
+                force: true,
+                operations: vec![
+                    ("foo".into(), "-t/foo".into()),
+                    ("-n".into(), "-t/-n".into()),
+                ],
+                ..App::default()
+            }
+        );
+        assert_eq!(
+            parse(&["-T", "--", "--", "-f"]).unwrap(),
+            App {
+                operations: vec![("--".into(), "-f".into()),],
+                ..App::default()
+            }
+        );
+        assert_eq!(
+            parse(&["-t", "foo", "--", "-f"]).unwrap(),
+            App {
+                operations: vec![("-f".into(), "foo/-f".into()),],
+                ..App::default()
+            }
+        );
+        assert_eq!(
+            parse(&["-t", "--", "-f"]).unwrap_err(),
+            "the '-t' option doesn't have an associated value"
+        );
     }
 }
